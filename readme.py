0dba5721dc6af998f737e25d63bdce69d898e468
sudomaze <mazen.ota@gmail.com>
2023-04-03 17:11:43 -0700

    change of sudomaze to ma7dev

--- readme.py
+++ readme.py
@@ -3,32 +3,32 @@ from rich.tree import Tree
 
 console = Console(record=True, width=100)
 
-tree = Tree("🙂 [link=https://sudomaze.dev]Mazen Alotaibi", guide_style="bold bright_black")
+tree = Tree("🙂 [link=https://ma7dev.dev]Mazen Alotaibi", guide_style="bold bright_black")
 
 python_tree = tree.add("📦 Open Source Packages", guide_style="bright_black")
 python_tree.add("[bold link=https://github.com/approx-ml/approx]approx[/] \t\t - [bright_black]automatic quantization library")
-python_tree.add("[bold link=https://github.com/sudomaze/onlysudo]onlysudo[/] \t - [bright_black]Discord/Twitch bot that uses AI models")
-python_tree.add("[bold link=https://github.com/sudomaze/pyreqpp]pyreqpp[/] \t - [bright_black]automatic update missing module versions in requirements.txt to latest.")
+python_tree.add("[bold link=https://github.com/ma7dev/onlysudo]onlysudo[/] \t - [bright_black]Discord/Twitch bot that uses AI models")
+python_tree.add("[bold link=https://github.com/ma7dev/pyreqpp]pyreqpp[/] \t - [bright_black]automatic update missing module versions in requirements.txt to latest.")
 
 contrib_tree = tree.add("🔬 Experiments", guide_style="bright_black")
-contrib_tree.add("[bold link=https://github.com/sudomaze/deep-learning-project-template]deep-learning-project-template[/] \t\t - [bright_black]PyTorch Lightning, Pydantic, and more!")
-contrib_tree.add("[bold link=https://github.com/sudomaze/custom_dataset]custom_dataset[/] \t\t\t\t - [bright_black]PyTorch dataset example for custom dataset")
-contrib_tree.add("[bold link=https://github.com/sudomaze/automate-experiments]automate-experiments[/] \t\t\t - [bright_black]automate experiments using GitHub Actions")
-contrib_tree.add("[bold link=https://github.com/sudomaze/dvcorg-mlops]dvcorg-mlops[/] \t\t\t\t - [bright_black]automate experiments using DVC")
-contrib_tree.add("[bold link=https://github.com/sudomaze/ttorch]ttorch[/] \t\t\t\t\t - [bright_black]a simple wrapper for PyTorch")
-contrib_tree.add("[bold link=https://github.com/sudomaze/benthos-example]benthos-example[/] \t\t\t - [bright_black]a simple example of using Benthos")
-contrib_tree.add("[bold link=https://github.com/sudomaze/cookiecutter-pytorch-lightning]cookiecutter-pytorch-lightning[/] \t\t - [bright_black]cookiecutter PyTorch Lightning")
-contrib_tree.add("[bold link=https://github.com/sudomaze/cookiecutter-pytorch-lightning-cluster]cookiecutter-pytorch-lightning-cluster[/] \t - [bright_black]cookiecutter PyTorch Lightning for cluster")
-contrib_tree.add("[bold link=https://github.com/sudomaze/when-my-internet-is-down]when-my-internet-is-down[/] \t\t - [bright_black]script to log when your interet is down")
-contrib_tree.add("[bold link=https://github.com/sudomaze/LaTeX-Templates]LaTeX-Templates[/] \t\t\t - [bright_black]a collection of LaTeX templates")
+contrib_tree.add("[bold link=https://github.com/ma7dev/deep-learning-project-template]deep-learning-project-template[/] \t\t - [bright_black]PyTorch Lightning, Pydantic, and more!")
+contrib_tree.add("[bold link=https://github.com/ma7dev/custom_dataset]custom_dataset[/] \t\t\t\t - [bright_black]PyTorch dataset example for custom dataset")
+contrib_tree.add("[bold link=https://github.com/ma7dev/automate-experiments]automate-experiments[/] \t\t\t - [bright_black]automate experiments using GitHub Actions")
+contrib_tree.add("[bold link=https://github.com/ma7dev/dvcorg-mlops]dvcorg-mlops[/] \t\t\t\t - [bright_black]automate experiments using DVC")
+contrib_tree.add("[bold link=https://github.com/ma7dev/ttorch]ttorch[/] \t\t\t\t\t - [bright_black]a simple wrapper for PyTorch")
+contrib_tree.add("[bold link=https://github.com/ma7dev/benthos-example]benthos-example[/] \t\t\t - [bright_black]a simple example of using Benthos")
+contrib_tree.add("[bold link=https://github.com/ma7dev/cookiecutter-pytorch-lightning]cookiecutter-pytorch-lightning[/] \t\t - [bright_black]cookiecutter PyTorch Lightning")
+contrib_tree.add("[bold link=https://github.com/ma7dev/cookiecutter-pytorch-lightning-cluster]cookiecutter-pytorch-lightning-cluster[/] \t - [bright_black]cookiecutter PyTorch Lightning for cluster")
+contrib_tree.add("[bold link=https://github.com/ma7dev/when-my-internet-is-down]when-my-internet-is-down[/] \t\t - [bright_black]script to log when your interet is down")
+contrib_tree.add("[bold link=https://github.com/ma7dev/LaTeX-Templates]LaTeX-Templates[/] \t\t\t - [bright_black]a collection of LaTeX templates")
 
 contrib_tree = tree.add("👍 Contributions", guide_style="bright_black")
-contrib_tree.add("[bold link=https://github.com/pytorch/serve/pulls?q=sudomaze]pytorch/serve[/] \t\t - [bright_black]contributed by adding features")
-contrib_tree.add("[bold link=https://github.com/pytorch/examples/pulls?q=sudomaze]pytorch/examples[/] \t - [bright_black]contributed by adding examples")
-# contrib_tree.add("[bold link=https://github.com/HomebrewNLP/HomebrewNLP-Jax/pulls?q=sudomaze]HomebrewNLP-Jax[/] \t - [bright_black]contributed by adding MLOps to automate experiments")
+contrib_tree.add("[bold link=https://github.com/pytorch/serve/pulls?q=ma7dev]pytorch/serve[/] \t\t - [bright_black]contributed by adding features")
+contrib_tree.add("[bold link=https://github.com/pytorch/examples/pulls?q=ma7dev]pytorch/examples[/] \t - [bright_black]contributed by adding examples")
+# contrib_tree.add("[bold link=https://github.com/HomebrewNLP/HomebrewNLP-Jax/pulls?q=ma7dev]HomebrewNLP-Jax[/] \t - [bright_black]contributed by adding MLOps to automate experiments")
 
 online_tree = tree.add("⭐ Online Projects", guide_style="bright_black")
-online_tree.add("[bold link=https://sudomaze.dev]sudomaze.dev[/] \t - [bright_black]personal blog")
+online_tree.add("[bold link=https://ma7dev.dev]ma7dev.dev[/] \t - [bright_black]personal blog")
 
 employer_tree = tree.add("👨‍💻 Employer", guide_style="bright_black")
 
@@ -50,7 +50,7 @@ rasa_tree.add("[bold]AI Web Demo[/] \t\t\t - [bright_black]sponsored by TechData
 
 console.print(tree)
 console.rule("[bold red]Socials")
-console.print("[green]Connect with me on linkedin [bold link=https://linkedin.com/in/sudomaze]Mazen Alotaibi[/].")
+console.print("[green]Connect with me on linkedin [bold link=https://linkedin.com/in/ma7dev]Mazen Alotaibi[/].")
 # line
 console.rule("[bold red]Thanks")
 console.print("[yellow]This is a fork from [bold link=https://github.com/koaning/koaning]Vincent D. Warmerdam (koaning)[/].")
